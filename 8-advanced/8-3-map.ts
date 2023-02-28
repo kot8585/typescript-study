d866d7f6d961a8c0941addb042aaeaef050887db
kot8585 <ansgywjd321@gmail.com>
2023-02-28 09:22:43 +0900

    mapped type
    신기방기

---
+++ 8-advanced/8-3-map.ts
@@ -0,0 +1,51 @@
+{
+  /**
+ * Mapped Type
+ * => 타입을 빙글빙글 돌면서 여러가지 변형된 타입을 만들어낼 수 있다. 
+ */
+  //예제 1️⃣ 
+  type Video = {
+    title: string;
+    author: string;
+  }
+
+  type Optional<T> = {
+    [P in keyof T]?: T[P]; // for ...in
+    /**
+     * T의 key들을 순회하면서 "Key?: Key의 타입"으로 만든다. 
+     */
+  };
+  type MappedVideoOptional = Optional<Video> //아래의 VideoOptional과 같음
+
+  type VideoOptional = {
+    title?: string;
+    author?: string;
+  }
+
+  // 이제는 optional이기에 모든 Key들을 주지 않아도 된다.
+  type Animal = {
+    name: string;
+    age: number;
+  }
+  const animal: Optional<Animal> = {
+    name: 'dog', // 모든 Key들을 주지 않아도 된다.
+  }
+
+  //예제 2: mapped type을 이용하여 readOnly로 만들어보자 
+  type ReadOnly<T> = {
+    readonly [P in keyof T]: T[P];
+  }
+
+  //예제 3 : Mapped Type 활용해보기
+  //TODO: 1. https://dev.to/mattzgg_94/typescript-use-mapped-type-to-implement-a-proxy-4im2 읽어보기
+  //TODO: 2. 사용해보기
+  type Proxy<T> = {
+    get(): T;
+    set(value: T): void;
+  };
+
+  type Proxify<T> = {
+    [P in keyof T]: Proxy<T[P]>
+  }
+
+}
